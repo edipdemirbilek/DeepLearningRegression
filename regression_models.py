6dcb33bcc507c2899d75dc462155ac18014403e9
Edip Demirbilek <edip.demirbilek@workjam.com>
2017-11-27 22:33:51 -0500

    updated argument parser and docstrings

--- regression_models.py
+++ regression_models.py
@@ -35,24 +35,27 @@ Usage Examples:
 
 Attributes:
     Common:
-        debug -- debug, boolean, default False
-        model_type -- string, dl/rf/bg, default dl
-        random_search -- boolean, default False
-        model_id -- string, default 1
-        num_models -- int, default 500
-        k -- int, default 4
-        n_features -- int, default 127
-        count -- int, default 3
+        verbose -- Verbose, default: disabled, optional
+        m_type -- Model type, string, options: 'dl', 'rf' or 'bg', required
+        random --  Random hyperparameter search, default: disabled, optional
+        model_id -- Custom model id, int, default: 1, optional
+        n_models -- Max number of models, int, default: 500, optional
+        k -- Cross validation k-fold value, int, default: 4, optional
+        n_features -- Number of features, int, default: 127, optional
+        count -- Repeat k-fold cross validation # times, int, default: 3,
+            optional
 
-    Deep Learning:
-        n_layer_max -- int, default 20
-        n_epoch_max -- int, default 2**14
-        n_batch_size -- int, default 120
-        dropout -- boolean, default random
-        k_l2 -- boolean, default random
-        k_l1 -- boolean, default random
-        a_l2 -- boolean, default random
-        a_l1 -- boolean, default random
+    Deep Learning: , only for random search
+        n_layer -- Max number of hidden layers, int, default: random(1, 20),
+            optional
+        n_epoch -- Max number of epochs, int, default: random(512,2^14),
+            optional
+        n_batch -- Batch size, int, default: 120, optional
+        dropout -- Dropout, default: random(True, False), optional
+        k_l2 -- Kernel L2 regularization, default: False, optional
+        k_l1 -- Kernel L1 regularization, default: True, optional
+        a_l2 -- Activation L2 regularization, default: False, optional
+        a_l1 -- Activation L1 regularization, default: False, optional
 
     Random Forests:
 
@@ -73,10 +76,6 @@ from deep_learning.dl_utils import pack_regularization_object
 from deep_learning.dl_utils import create_model, run_model
 from deep_learning.dl_models import create_model_1
 
-# for k-Fold cross validation
-K = 4
-NUM_FEATURES = 127
-
 
 def build_parser():
     """Example function with PEP 484 type annotations.
@@ -90,12 +89,55 @@ def build_parser():
 
     """
     parser = ArgumentParser()
-    parser.add_argument('--debug', dest='debug',
-                        help='debug level',
-                        metavar='DEBUG', required=False)
-    parser.add_argument('--model_type', dest='model_type',
-                        help='model type(random, custom)',
-                        metavar='MODEL TYPE', required=False)
+
+    # common parameters
+    parser.add_argument('-v', '--verbose',
+                        help='increase output verbosity',
+                        action="store_true")
+    parser.add_argument('--m_type', required=True,
+                        choices=['dl', 'rf', 'bg'],
+                        help='model type: deep learning, random forests or \
+                        bagging')
+    parser.add_argument('--random', action="store_true", help="random search \
+                        over hyperparameters")
+    parser.add_argument('--model_id', type=int, default=1,
+                        help="custom model id")
+    parser.add_argument('--n_models', type=int, default=500, help="max number of \
+                        models to create during random search over \
+                        hyperparameters")
+    parser.add_argument('--k', type=int, default=4,
+                        help="cross validation k-fold value")
+    parser.add_argument('--n_features', type=int, choices=["1 - 127"],
+                        default=127, help="number of features")
+    parser.add_argument('--count', type=int, default=3,
+                        help="repeat 'count' times k-fold cross validation on \
+                        the same model")
+
+    # Deep Learning Parameters
+    parser.add_argument('--n_layer', type=int, default=random.randint(1, 20),
+                        choices=["1 - 20"],
+                        help="max number of hidden layers")
+    parser.add_argument('--n_epoch', type=int,
+                        default=int(power(2, 14 * uniform(0.642, 1.0))),
+                        choices=["1 - 2^14"], help='max number of \
+                        epochs')
+    parser.add_argument('--n_batch', choices=["1-127"], default=120,
+                        help='batch number')
+    parser.add_argument('--dropout', action="store_true",
+                        default=random.choice([True, False]),
+                        help="apply dropout")
+    parser.add_argument('--k_l2', action="store_true",
+                        default=False,
+                        help='kernel L2 regularization')
+    parser.add_argument('--k_l1', action="store_true",
+                        default=True,
+                        help='kernel L1 regularization')
+    parser.add_argument('--a_l2', action="store_true",
+                        default=False,
+                        help='activation L2 regularization')
+    parser.add_argument('--a_l1', action="store_true",
+                        default=False,
+                        help='activation L1 regularization')
     return parser
 
 
@@ -111,39 +153,42 @@ def main():
 
     """
     parser = build_parser()
-    options = parser.parse_args()
+    args = parser.parse_args()
 
     attributes, labels = load_dataset()
 
-    model_type_is_random = options.model_type == "random"
+    if args.m_type == "dl":
+        if args.random:
+            # save_resultsHeader()
 
-    if model_type_is_random:
-        # save_resultsHeader()
-        # running the same test count times
-        count = 3
-        # For Random Search Hyperparameter exploration
-        for i in range(1, 500):
-            # n_layers = int(power(2,4*np.random.rand()))
-            n_layers = random.randint(1, 20)
-            n_epoch = int(power(2, 14 * uniform(0.642, 1.0)))
-            n_batch_size = 120
-            test_id = str(i)+str(rand())
-            regularization = pack_regularization_object(
-                dropout=random.choice([True, False]),
-                k_l2=False, k_l1=True, a_l2=False, a_l1=False)
-            dl_model = create_model(n_layers, NUM_FEATURES, regularization)
-            run_model(attributes, labels, test_id, dl_model, count, K,
-                      NUM_FEATURES, n_layers, n_epoch, n_batch_size,
-                      regularization, debug=options.debug)
+            # For Random Search Hyperparameter exploration
+            for i in range(1, args.n_models):
+                test_id = str(i)+str(rand())
+
+                regularization = pack_regularization_object(
+                        dropout=args.dropout, k_l2=args.k_l2, k_l1=args.k_l1,
+                        a_l2=args.a_l2, a_l1=args.a_l1)
+
+                dl_model = create_model(args.n_layer, args.n_features,
+                                        regularization)
+
+                run_model(attributes, labels, test_id, dl_model, args.count,
+                          args.k, args.n_features, args.n_layer, args.n_epoch,
+                          args.n_batch, regularization, verbose=args.verbose)
+        else:
+
+            create_custom_model = {1: create_model_1, }
+
+            test_id = str("custom") + str(rand())
+
+            dl_model, n_layers, n_epoch, n_batch_size, regularization \
+                = create_custom_model[args.model_id](args.n_features)
+            run_model(
+                attributes, labels, test_id, dl_model, args.count, args.k,
+                args.n_features, n_layers, n_epoch, n_batch_size,
+                regularization, verbose=args.verbose)
     else:
-        count = 1
-        test_id = str("custom") + str(rand())
-        dl_model, n_layers, n_epoch, n_batch_size, regularization \
-            = create_model_1(NUM_FEATURES)
-        run_model(
-            attributes, labels, test_id, dl_model, count, K, NUM_FEATURES,
-            n_layers, n_epoch, n_batch_size, regularization,
-            debug=options.debug)
+        raise Exception('Not Implemented')
 
 if __name__ == '__main__':
     main()
